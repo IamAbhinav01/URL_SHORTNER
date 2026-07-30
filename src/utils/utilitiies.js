e639c29360f7828b13c046e6d6b035bf6ac1d546
Abhinav <AbhinavSunil@hotmail.com>
2026-07-30 11:23:35 +0530

    Feat : Added the utilities

--- src/utils/utilitiies.js
+++ src/utils/utilitiies.js
@@ -1,12 +1,54 @@
 const { BIGINT } = require('sequelize');
+const crypto = require('crypto');
 const { BASE62_ALPHABET } = require('../config/server.config');
 
-const base62_alpha = BASE62_ALPHABET;
-
 const encodeBase62 = (num) => {
-  if (num == 0n) return base62_alpha[0];
+  if (num == 0n) return BASE62_ALPHABET[0];
 
   let result = '';
   let current = BIGINT(num);
   const base = 62n;
+
+  while (current > 0n) {
+    const remainder = Number(current % base);
+    result = BASE62_ALPHABET[remainder] + result;
+    current = current / base;
+  }
+  return result;
+};
+
+const decodeBase62 = (str) => {
+  let result = 0n;
+  const base = 62n;
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const index = BigInt(BASE62_ALPHABET.indexOf(char));
+    if (index === -1n) throw new Error(`Invalid Base62 character: ${char}`);
+    result = result * base + index;
+  }
+
+  return result;
+};
+
+const generateShortCode = (length = 6) => {
+  const bytes = crypto.randomBytes(6);
+  // Convert 6 random bytes into a 48-bit unsigned BigInt
+  const randomNum = BigInt(`0x${bytes.toString('hex')}`);
+
+  let code = encodeBase62(randomNum);
+
+  if (code.length < length) {
+    code = code.padStart(length, '0');
+  } else if (code.length > length) {
+    code = code.slice(0, length);
+  }
+
+  return code;
+};
+
+module.exports = {
+  encodeBase62,
+  decodeBase62,
+  generateShortCode,
 };
