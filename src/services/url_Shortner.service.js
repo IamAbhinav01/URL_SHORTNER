36582be6e85e4d0985e6a7146f89624467c8471d
Abhinav <AbhinavSunil@hotmail.com>
2026-07-30 22:59:41 +0530

    Feat : added the auto incrementer and db and redis cache checker

--- src/services/url_Shortner.service.js
+++ src/services/url_Shortner.service.js
@@ -3,6 +3,8 @@ const { ErrorHandler } = require('../errors');
 const { generateShortCode } = require('../utils/utilitiies');
 const { UrlMapping } = require('../models');
 const { redisConfig } = require('../config');
+const urlmapping = require('../models/urlmapping');
+const { where } = require('sequelize');
 
 const CACHE_TTL_SECONDS = 86400;
 
@@ -34,5 +36,37 @@ class UrlShortner {
     }
   }
 
-  static async resolverUrl(shortCode) {}
+  static async resolverUrl(shortCode) {
+    const cachedUrl = await redisConfig.get(`url:${shortCode}`);
+    if (cachedUrl) {
+      // ithu redis il check cheyum
+      this.incrementClicks(shortCode).catch(console.error);
+      return cachedUrl;
+    }
+    //eni dbyil  nokanam
+    const mapping = await urlmapping.findOne({
+      where: { shortCode },
+    });
+
+    if (!mapping) return null;
+
+    //add to redis
+    await redisConfig.client.setEx(
+      `url:${shortCode}`,
+      CACHE_TTL_SECONDS,
+      mapping.orginalUrl
+    );
+    this.incrementClicks(shortCode).catch(console.error);
+
+    return mapping.orginalUrl;
+  }
+
+  static async incrementClicks(shortCode) {
+    await urlmapping.increment('clickCount', {
+      by: 1,
+      where: { shortCode },
+    });
+  }
 }
+
+module.exports = { UrlShortner };
