1f7cb11b7dd9a15ef525247f7f1e3db4eb358b6c
Abhinav <AbhinavSunil@hotmail.com>
2026-07-30 22:50:03 +0530

    Feat:Implement shorturl maker, now resolver needs to be implemented

--- src/services/url_Shortner.service.js
+++ src/services/url_Shortner.service.js
@@ -8,18 +8,31 @@ const CACHE_TTL_SECONDS = 86400;
 
 class UrlShortner {
   static async createShortUrl(orginalUrl) {
-    shortCode = generateShortCode(6);
-    try {
-      await UrlMapping.create({ shortCode, orginalUrl });
-      await redisConfig.client.setEx(
-        `url:${shortCode}`,
-        CACHE_TTL_SECONDS,
-        orginalUrl
+    let tries = 0;
+    const maxAttempts = 5;
+    while (tries < maxAttempts) {
+      shortCode = generateShortCode(6);
+      try {
+        await UrlMapping.create({ shortCode, orginalUrl });
+        await redisConfig.client.setEx(
+          `url:${shortCode}`,
+          CACHE_TTL_SECONDS,
+          orginalUrl
+        );
+        return shortCode;
+      } catch (error) {
+        loggerConfig.error('Error while creating shortUrl');
+        if (error.name === 'SequelizeUniqueConstraintError') {
+          tries++;
+          continue;
+        }
+        throw error;
+      }
+      throw new ErrorHandler(
+        'Failed to generate a unique short code after maximum retries'
       );
-      return shortCode;
-    } catch (error) {
-      loggerConfig.error('Error while creating shortUrl');
-      throw error;
     }
   }
+
+  static async resolverUrl(shortCode) {}
 }
