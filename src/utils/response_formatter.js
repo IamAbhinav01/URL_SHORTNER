b82728a5851de9a82891b6631d55a08b1f65bdeb
Abhinav <AbhinavSunil@hotmail.com>
2026-07-30 23:17:12 +0530

    Feat : implemented response formatter

---
+++ src/utils/response_formatter.js
@@ -0,0 +1,18 @@
+const succesResponse = {
+  success: true,
+  data: {},
+  error: {},
+  message: 'message has sent successfully',
+};
+
+const errorResponse = {
+  success: false,
+  data: {},
+  error: {},
+  message: 'message has failed',
+};
+
+module.exports = {
+  succesResponse,
+  errorResponse,
+};
