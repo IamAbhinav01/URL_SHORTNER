bfe95541bd2e50f62b5d5a64bfe9c94bdf8a5608
Abhinav <AbhinavSunil@hotmail.com>
2026-07-30 15:31:19 +0530

    Feat : working on storing the shortcode and orginalUrl to db and memory

--- src/services/url_Shortner.service.js
+++ src/services/url_Shortner.service.js
@@ -1,1 +1,14 @@
-class UrlShortner {}
+const { loggerConfig } = require('../config/');
+const { ErrorHandler } = require('../errors');
+
+class UrlShortner {
+  static async createShortUrl(orginalUrl) {
+    try {
+    } catch (error) {
+      loggerConfig.error('Error while creating shortUrl');
+      throw ErrorHandler(
+        'Failed to generate a unique short code after maximum retries'
+      );
+    }
+  }
+}
